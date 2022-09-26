0aaf332d68e2ddf5bc7456b7c941054ec0b76af5
Levy <levyronald3@gmail.com>
2022-09-26 10:56:35 -0300

    feat: criando componente de modal e puxando na tela de usuários

---
+++ src/components/Modals/UserCreate.vue
@@ -0,0 +1,43 @@
+<template>
+  <b-modal
+    id="modal-create"
+    cancel-variant="outline-secondary"
+    ok-title="Login"
+    cancel-title="Close"
+    title="Login Form"
+    :visible="isAddNewUserModalActive"
+    @change="(val) => ('update:is-add-new-user-modal-active', val)"
+  >
+    <b-form>
+      <b-form-group>
+        <label for="email">Email:</label>
+        <b-form-input id="email" type="email" placeholder="Email Address" />
+      </b-form-group>
+      <b-form-group>
+        <label for="password">Password</label>
+        <b-form-input type="password" placeholder="Password" />
+      </b-form-group>
+    </b-form>
+  </b-modal>
+</template>
+<script>
+import { VBModal } from "bootstrap-vue";
+export default {
+  components: {
+    VBModal
+  },
+  directives: {
+    "b-modal": VBModal,
+  },
+  model: {
+    prop: 'isAddNewUserModalActive',
+    event: 'update:is-add-new-user-modal-active',
+  },
+  props: {
+    isAddNewUserModalActive: {
+      type: Boolean,
+      required: true,
+    },
+  }
+};
+</script>
--- src/pages/Users/users.vue
+++ src/pages/Users/users.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="content">
+    <user-create-vue :is-add-new-user-modal-active.sync="isAddNewUserModalActive" />
     <div class="container-fluid">
       <div class="row">
         <div class="w-100">
@@ -11,14 +12,22 @@
               <div class="row">
                 <div class="col-md-7">
                   <h4 class="card-title">Usuários cadastrados</h4>
-                  <p class="card-category">Lista de usuários cadastrados na tallosflix</p>
+                  <p class="card-category">
+                    Lista de usuários cadastrados na tallosflix
+                  </p>
                 </div>
-                <div class="col-md-5 d-flex align-items-center justify-content-end">
-                  <button class="btn btn-primary h-75" type="submit">
-                  <span class="text-nowrap">Adicionar</span>
-                </button>
-                <div class="pl-1">
-                  <b-form-input debounce="300" v-model="filter" placeholder="pesquisar..."/>
+                <div
+                  class="col-md-5 d-flex align-items-center justify-content-end"
+                >
+                  <button class="btn btn-primary h-75" type="submit" v-b-modal.modal-create>
+                    <span class="text-nowrap">Adicionar</span>
+                  </button>
+                  <div class="pl-1">
+                    <b-form-input
+                      debounce="300"
+                      v-model="filter"
+                      placeholder="pesquisar..."
+                    />
                   </div>
                 </div>
               </div>
@@ -38,7 +47,10 @@
               <template #cell(actions)="">
                 <b-dropdown no-caret variant="flat">
                   <template #button-content>
-                    <b-icon icon="three-dots-vertical" class="align-middle"></b-icon>
+                    <b-icon
+                      icon="three-dots-vertical"
+                      class="align-middle"
+                    ></b-icon>
                   </template>
                   <b-dropdown-item>
                     <b-icon icon="box-arrow-up-right" scale="0.9"></b-icon>
@@ -58,37 +70,49 @@
   </div>
 </template>
 <script>
+import {
+ VBModal, 
+} from 'bootstrap-vue'
 import Card from "../../components/Cards/Card.vue";
-import store from "../../store/index.js"
+import store from "../../store/index.js";
+import UserCreateVue from '../../components/Modals/UserCreate.vue';
+import { ref } from 'vue';
 export default {
   components: {
-    Card
+    Card,
+    VBModal,
+    UserCreateVue
   },
   data() {
     return {
       totalRows: 1,
       currentPage: 1,
       filter: null,
+      isAddNewUserModalActive: ref(false),
       filterOn: [],
       users: [],
       column: [
-        { key: "_id"},
+        { key: "_id" },
         { key: "name", label: "nome" },
         { key: "email", label: "email" },
-        { key: "actions", label: "ações"}
-      ]
-    }
+        { key: "actions", label: "ações" },
+      ],
+    };
+  },
+  directives: {
+    'b-modal': VBModal
   },
   mounted() {
-    store.dispatch("getUsers")
-    .then((response) => (this.users = response.data))
-    .catch((erro) => console.log(erro));
+    store
+      .dispatch("getUsers")
+      .then((response) => (this.users = response.data))
+      .catch((erro) => console.log(erro));
   },
   methods: {
     onFiltered(filteredItems) {
       this.totalRows = filteredItems.length;
       this.currentPage = 1;
     },
-  }
+  },
 };
 </script>
