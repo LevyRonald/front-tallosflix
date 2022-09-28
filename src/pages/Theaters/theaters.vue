90839d0eb8e91645fce4880a95fcab0fa982426a
Levy <levyronald3@gmail.com>
2022-09-28 11:24:13 -0300

    feat:  header da página de teatros concluído

--- src/pages/Theaters/theaters.vue
+++ src/pages/Theaters/theaters.vue
@@ -1,3 +1,43 @@
 <template>
-    <div>pagina de teatros</div>
+  <div class="content">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="w-100">
+          <card
+            class="strpied-tabled-with-hover"
+            body-classes="table-full-width table-responsive"
+          >
+            <template slot="header">
+              <div class="row">
+                <div class="col-md-7">
+                  <h4 class="card-title">Teatros Cadastrados</h4>
+                  <p class="card-category">
+                    Lista de Teatros cadastrados na tallosflix
+                  </p>
+                </div>
+                <div
+                  class="col-md-5 d-flex align-items-center justify-content-end"
+                >
+                  <button class="btn btn-primary h-75" type="submit">
+                    <span class="text-nowrap">Adicionar</span>
+                  </button>
+                  <div class="pl-1">
+                    <b-form-input debounce="300" placeholder="pesquisar..."/>
+                  </div>
+                </div>
+              </div>
+            </template>
+          </card>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+<script>
+import Card from "../../components/Cards/Card.vue";
+export default {
+  components: { Card },
+};
+</script>
+
+Card
