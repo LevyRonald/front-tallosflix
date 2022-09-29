2c8ff445515c447994d6ff61eb2b555d9d63463a
Levy <levyronald3@gmail.com>
2022-09-29 14:12:02 -0300

    feat header da pagina de movies

--- src/pages/Movies/movies.vue
+++ src/pages/Movies/movies.vue
@@ -1,3 +1,45 @@
 <template>
-    <div>tela de filmes</div>
+  <div class="content">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="w-100">
+          <card
+            class="strpied-tabled-with-hover"
+            body-classes="table-full-width table-responsive"
+          >
+            <template slot="header">
+              <div class="row">
+                <div class="col-md-7">
+                  <h4 class="card-title">Filmes cadastrados</h4>
+                  <p class="card-category">
+                    Lista de filmes cadastrados na tallosflix
+                  </p>
+                </div>
+                <div
+                  class="col-md-5 d-flex align-items-center justify-content-end"
+                >
+                  <button class="btn btn-primary h-75" type="submit">
+                    <span class="text-nowrap">Adicionar</span>
+                  </button>
+                  <div class="pl-1">
+                    <b-form-input
+                      debounce="300"
+                      placeholder="pesquisar..."
+                    ></b-form-input>
+                  </div>
+                </div>
+              </div>
+            </template>
+          </card>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+<script>
+import Card from "../../components/Cards/Card.vue";
+
+export default {
+  components: { Card },
+};
+</script>
