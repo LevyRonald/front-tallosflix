7616f09bbbe12412c2310028673a2e4fd8548116
Levy <levyronald3@gmail.com>
2022-10-04 15:31:01 -0300

    feat: exibindo page de comments na pagina de logs

--- src/pages/Logs/Logs.vue
+++ src/pages/Logs/Logs.vue
@@ -6,7 +6,7 @@
           <b-tabs align="center">
             <b-card>
               <b-tab title="Sessões" active><sessions/></b-tab>
-            <b-tab title="Comentários"></b-tab>
+            <b-tab title="Comentários"><comments/></b-tab>
             </b-card>
           </b-tabs>
         </div>
@@ -16,8 +16,9 @@
 </template>
 <script>
 import Sessions from "../../components/Logs/sessions.vue";
+import Comments from "../../components/Logs/comments.vue";
 import Card from "../../components/Cards/Card.vue";
 export default {
-  components: { Card, Sessions },
+  components: { Card, Sessions, Comments },
 };
 </script>
