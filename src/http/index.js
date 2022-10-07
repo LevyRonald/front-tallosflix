b45e4ea4601b9c7cb6ef152a105001452cae27c5
Levy <levyronald3@gmail.com>
2022-10-07 15:53:24 -0300

    fix: alterando url geral

--- src/http/index.js
+++ src/http/index.js
@@ -1,8 +1,7 @@
 import axios from 'axios'
 import store from '../store/index.js'
 const http = axios.create({
-    baseURL: 'http://localhost:3000',
-    // http://150.230.78.209:18000/le
+    baseURL: 'http://150.230.78.209:18000/le',
     headers: {
         'accept': 'application/json',
         'content': 'application/json'
